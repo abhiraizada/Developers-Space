1069f5c8eb48f7b497c4cc31807576484e7bfa61
Abhimanyu Raizada <abhi.raizada21@gmail.com>
2020-12-22 23:27:14 +0530

    react app build started

--- config/db.js
+++ config/db.js
@@ -4,7 +4,11 @@ const db = config.get('mongoURI');
 
 const connectDb = async() =>{
     try{
-        await mongoose.connect(db,{useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true});
+        await mongoose.connect(db,{
+            useNewUrlParser: true, 
+            useUnifiedTopology: true, 
+            useCreateIndex: true
+        });
         console.log('Db Connected');
     }
     catch(err){
